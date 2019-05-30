83f508251f281d0d2d97877a6165217deda6eb42
aancjr <45945137+aancjr@users.noreply.github.com>
2019-05-29 20:40:45 -0400

    added require files/variables, api variables and keys, and variable to take in user command

--- liri.js
+++ liri.js
@@ -1,10 +1,17 @@
-//require the .env file
+//require files:
 require("dotenv").config();
-
-//requireing the keys.js file
+var fs = require("fs");
 var keys = require("./keys.js");
 
 //access keys information
 var spotify = new spotify(keys.spotify);
 
+//take in user command
 var command = process.argv[2];
+
+//Bands int Town Artist Events API
+var artist = process.argv[3];
+var bands = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+//OMDB API
+var movies = "http://www.omdbapi.com/?apikey=trilogy&"
