5b2b7fe96d2be2926698a856c096e6c329569b23
aancjr <45945137+aancjr@users.noreply.github.com>
2019-05-29 20:56:11 -0400

    added declared variable to require axios

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 //require files:
 require("dotenv").config();
+var axios = require("axios");
 var fs = require("fs");
 var keys = require("./keys.js");
 
@@ -10,8 +11,14 @@ var spotify = new spotify(keys.spotify);
 var command = process.argv[2];
 
 //Bands int Town Artist Events API
+   // possibly declare artist in if statement?
 var artist = process.argv[3];
 var bands = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
 //OMDB API
 var movies = "http://www.omdbapi.com/?apikey=trilogy&"
+
+//node liri.js concert-this <artist/band name here>
+if (command == "concert-this") {
+
+}
