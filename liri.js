0524c64d7394fea65191247814d8eb461a55b58e
aancjr <45945137+aancjr@users.noreply.github.com>
2019-05-30 18:35:50 -0400

    OMDB is working. logging info needed, just need to add additional text. other commands need work

--- liri.js
+++ liri.js
@@ -13,11 +13,11 @@ var command = process.argv[2];
 
 //Bands int Town Artist Events API
    // possibly declare artist in if statement?
-var artist = process.argv[3];
-var bands = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+var parameter = process.argv[3];
+var bands = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp"
 
 //OMDB API
-var movies = "http://www.omdbapi.com/?apikey=trilogy&"
+var movies = "http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy&"
 
 //node liri.js concert-this <artist/band name here>
     //NEED:
@@ -25,9 +25,27 @@ var movies = "http://www.omdbapi.com/?apikey=trilogy&"
         //venue location
         //date of the event (using moment to format as "MM/DD/YYYY")
 
-if (command == "concert-this") {
-    axios.get(bands).then(function(response){
-        console.log(response.data)
-        console.log("It works!")
+// if (command == "concert-this") {
+//     axios.get(bands).then(function(response){
+//         console.log(response.data)
+//     })
+// } 
+
+
+// node liri.js movie-this <movie name here>
+    //NEED:
+        // Title of movie, Year, IMDB Rating, Rotten Tomatoes Rating, Country Producted, language, Plot, Actors
+if (command == "movie-this") {
+    axios.get(movies).then(function(response) {
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.imdbRating);
+        console.log(response.data.Ratings[2]);
+        console.log(response.data.Country);
+        console.log(response.data.Language);
+        console.log(response.data.Plot);
+        console.log(response.data.Actors);
+
+        console.log("itworks!")
     })
 }
