db45ee6e7d465999596001d486305379aed8812e
aancjr <45945137+aancjr@users.noreply.github.com>
2019-06-02 10:59:45 -0400

    added moment to date stamp for concert this. added a.catch for movie-this

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var axios = require("axios");
 var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+var moment = require("moment");
 
 //access keys information
 var spotify = new Spotify(keys.spotify);
@@ -18,11 +19,10 @@ var bands = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_i
 
 //OMDB API
 var movies = "http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy&"
+var movieErr = "http://www.omdbapi.com/?t=movie+43&y=&plot=short&apikey=trilogy&"
 
 // node liri.js concert-this <artist/band name here>
 //     NEED:
-//         Name of Venue
-//         venue location
 //         date of the event (using moment to format as "MM/DD/YYYY")
 
 var concertThis = function() {
@@ -31,7 +31,7 @@ if (command == "concert-this") {
         console.log("-------------");
         console.log("Name of Venue: " + response.data[0].venue.name);
         console.log("Venue Location: " + response.data[0].venue.country);
-        console.log("Date of Even: " + response.data[0].datetime);
+        console.log("Date of Event: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
         console.log("-------------");
     }).catch(function(err) {
         console.log("CHECK YOUR SPELLING!");
@@ -42,40 +42,50 @@ if (command == "concert-this") {
 var movieThis = function() {
 if (command == "movie-this") {
     axios.get(movies).then(function(response) {
+        console.log("\n");
         console.log("Movie Title: " + response.data.Title);
         console.log("Movie Year: " + response.data.Year);
         console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes:" + response.data.Ratings[2]);
+        console.log("Rotten Tomatoes:" + response.data.Ratings[1]);
         console.log("Movie Country: " + response.data.Country);
         console.log("Movie Language: " + response.data.Language);
         console.log("Plot: " + response.data.Plot);
         console.log("Actors: " + response.data.Actors);
+        console.log("\n");
+    }).catch(function(){ 
+        axios.get(movieErr).then(function(response){
+            console.log("\n");
+            console.log("Movie Title: " + response.data.Title);
+            console.log("Movie Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes:" + response.data.Ratings[1]);
+            console.log("Movie Country: " + response.data.Country);
+            console.log("Movie Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("\n");
+        })
     })
 }
 }
 
-// node liri.js spotify-this-song '<song name here>'
-    //NEED:
-        //Artist(s), song's name, preview link of the song from Spotify, album.
-            //*If no song provided, default to "The Sign" by Ace of Base.
 var spotifyThis = function() {
 if (command == "spotify-this-song") {
     spotify.search({ type: 'track', query: parameter})
     .then(function(data) {
         console.log("\n");
-        console.log("Artist(s): " + data.tracks.items[0].artists[0].name)
+        console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
         console.log("Song Name: " + data.tracks.items[0].name);
         console.log("Spotify Song Preview: " + data.tracks.items[0].preview_url);
         console.log("Album Title: " + data.tracks.items[0].album.name);
-    }).catch(function(err) {
-        spotify.search({ type: 'track', query: 'The Sign'})
-        .then(function(data) {
-        console.log("--------------");
-        console.log("Artist(s): " + data.tracks.items[6].artists[0].name)
-        console.log("Song Name: " + data.tracks.items[6].name);
-        console.log("Spotify Song Preview: " + data.tracks.items[6].preview_url);
-        console.log("Album Title: " + data.tracks.items[6].album.name);
-        console.log("--------------");
+    }).catch(function() {
+        spotify.search({ type: 'track', query: 'The Sign'}).then(function(data) {
+            console.log("--------------");
+            console.log("Artist(s): " + data.tracks.items[6].artists[0].name);
+            console.log("Song Name: " + data.tracks.items[6].name);
+            console.log("Spotify Song Preview: " + data.tracks.items[6].preview_url);
+            console.log("Album Title: " + data.tracks.items[6].album.name);
+            console.log("--------------");
     }) 
   });
 }}
@@ -90,8 +100,8 @@ if (command == "do-what-it-says") {
             return console.log(err);
         }
 
-        var dataArr = data.split(",")
-        console.log(dataArr)
+        var dataArr = data.split(",");
+        console.log(dataArr);
 
     })
 }
