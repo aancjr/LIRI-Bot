182e81c2701fd3234f5bb124a3038b993a9c5f58
aancjr <45945137+aancjr@users.noreply.github.com>
2019-06-02 09:56:38 -0400

    spotify this command shows correct info and populates the sign by ace of base upon an error

--- liri.js
+++ liri.js
@@ -10,10 +10,10 @@ var spotify = new Spotify(keys.spotify);
 
 //take in user command
 var command = process.argv[2];
+var parameter = process.argv[3];
 
 //Bands in Town Artist Events API
    // possibly declare artist in if statement?
-var parameter = process.argv[3];
 var bands = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp"
 
 //OMDB API
@@ -25,6 +25,7 @@ var movies = "http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=t
 //         venue location
 //         date of the event (using moment to format as "MM/DD/YYYY")
 
+var concertThis = function() {
 if (command == "concert-this") {
     axios.get(bands).then(function(response){
         console.log("-------------");
@@ -35,54 +36,68 @@ if (command == "concert-this") {
     }).catch(function(err) {
         console.log("CHECK YOUR SPELLING!");
     })
-} 
+}
+}
 
-
-// if (command == "movie-this") {
-//     axios.get(movies).then(function(response) {
-//         console.log("Movie Title: " + response.data.Title);
-//         console.log("Movie Year: " + response.data.Year);
-//         console.log("IMDB Rating: " + response.data.imdbRating);
-//         console.log("Rotten Tomatoes:" + response.data.Ratings[2]);
-//         console.log("Movie Country: " + response.data.Country);
-//         console.log("Movie Language: " + response.data.Language);
-//         console.log("Plot: " + response.data.Plot);
-//         console.log("Actors: " + response.data.Actors);
-//     })
-
+var movieThis = function() {
+if (command == "movie-this") {
+    axios.get(movies).then(function(response) {
+        console.log("Movie Title: " + response.data.Title);
+        console.log("Movie Year: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes:" + response.data.Ratings[2]);
+        console.log("Movie Country: " + response.data.Country);
+        console.log("Movie Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+    })
+}
+}
 
 // node liri.js spotify-this-song '<song name here>'
     //NEED:
         //Artist(s), song's name, preview link of the song from Spotify, album.
             //*If no song provided, default to "The Sign" by Ace of Base.
-  if (command == "spotify-this-song") {
+var spotifyThis = function() {
+if (command == "spotify-this-song") {
     spotify.search({ type: 'track', query: parameter})
     .then(function(data) {
-        console.log("--------------");
+        console.log("\n");
         console.log("Artist(s): " + data.tracks.items[0].artists[0].name)
         console.log("Song Name: " + data.tracks.items[0].name);
         console.log("Spotify Song Preview: " + data.tracks.items[0].preview_url);
         console.log("Album Title: " + data.tracks.items[0].album.name);
+    }).catch(function(err) {
+        spotify.search({ type: 'track', query: 'The Sign'})
+        .then(function(data) {
         console.log("--------------");
-    })
-//   .catch(function(err) {
-//     spotify.search({ type: 'track', query: 'The Sign', limit: 40 })
-//     .then(function(data) {
-//             console.log(data.tracks)
-// //         console.log("--------------");
-//         // console.log("Song Name: " + data.albums.items[1].artists[0]);
-// //         console.log("Spotify Song Preview: " + data.tracks.items[0].album.external_urls.spotify);
-// //         console.log("Album Title: " + data.tracks.items[0].album.name);
-// //         console.log("--------------");
-//     }) 
-//   });
-}
+        console.log("Artist(s): " + data.tracks.items[6].artists[0].name)
+        console.log("Song Name: " + data.tracks.items[6].name);
+        console.log("Spotify Song Preview: " + data.tracks.items[6].preview_url);
+        console.log("Album Title: " + data.tracks.items[6].album.name);
+        console.log("--------------");
+    }) 
+  });
+}}
 
 //node liri.js do-what-it-says
     //use fs Node package to run the random.txt file
 
+var doWhatItSays = function() {    
 if (command == "do-what-it-says") {
-    
+    fs.readFile("random.txt", "utf-8", function(err, data){ 
+        if (err) {
+            return console.log(err);
+        }
+
+        var dataArr = data.split(",")
+        console.log(dataArr)
+
+    })
+}
 }
 
-
+spotifyThis();
+movieThis();
+concertThis();
+doWhatItSays();
